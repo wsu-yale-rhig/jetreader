319c6e80494c985817a97c4661393ff0568b8623
Nick Elsey <nicholas.elsey@gmail.com>
2019-07-22 11:32:20 -0400

    update centrality definition for Run14LowMid

--- jetreader/reader/centrality_def.cc
+++ jetreader/reader/centrality_def.cc
@@ -16,18 +16,18 @@ CentralityDef::CentralityDef() {
   vz_range_.insert({CentDefId::Run14LowMid, {-30.0, 30.0}});
   zdc_norm_.insert({CentDefId::Run14LowMid, 30000.0});
   vz_norm_.insert({CentDefId::Run14LowMid, 0.0});
-  zdc_par_.insert({CentDefId::Run14LowMid, {175.758, -0.307738}});
+  zdc_par_.insert({CentDefId::Run14LowMid, {188.392, -0.32269}});
   weight_bound_.insert({CentDefId::Run14LowMid, 400});
 
   vz_par_.insert({CentDefId::Run14LowMid,
-                  {529.051, 0.192153, 0.00485177, -0.00017741, -1.44156e-05,
-                   3.97255e-07, -6.80378e-10}});
+                  {529.123, 0.19706, 0.00433184, -0.000183687, -1.29087e-05,
+                   3.82464e-07, -1.70998e-09}});
   weight_pars_.insert({CentDefId::Run14LowMid,
-                       {1.22692, -2.04056, 1.53857, 1.55649, -0.00123008,
-                        193.648, 1.30923e-06}});
+                       {1.34842, -12.8629, 0.767038, 4.2547, -0.00264771,
+                        357.779, 5.10897e-06}});
   cent_bounds_.insert({CentDefId::Run14LowMid,
-                       {7, 10, 15, 22, 31, 43, 58, 77, 100, 129, 163, 203, 249,
-                        303, 366, 441}});
+                       {10, 15, 22, 30, 42, 56, 74, 94, 120, 149, 184, 224, 269,
+                        321, 381, 450}});
 }
 
 } // namespace jetreader
